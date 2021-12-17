0338e4f9c710fd198c30e6c3b7d187fa66a9a7b5
KartikChippalkati <karucmc@gmail.com>
2021-12-18 02:33:50 +0530

    Simple Quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//import 'question.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
+  //const Quizzler({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Colors.grey[900],
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
+            child: MyApp(),
+          ),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(
+            "The Game Of Questions!!!",
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
           ),
         ),
       ),
@@ -19,26 +33,65 @@ class Quizzler extends StatelessWidget {
   }
 }
 
-class QuizPage extends StatefulWidget {
+class MyApp extends StatefulWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
   @override
-  _QuizPageState createState() => _QuizPageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _QuizPageState extends State<QuizPage> {
+class _MyAppState extends State<MyApp> {
+  List<Icon> ScoreKeeper = [];
+
+  //Question q1 = Question('is tajmahal is in delhi?',false);
+
+  // List<Question> _QuestionBank = [
+  //Question('is tajmahal is in delhi?',false),
+  // Question('is redfort is in delhi?',true),
+  //];
+
+  QuizBrain quizBrain = QuizBrain();
+
+  void checkAnswer(userClickedAnswer) {
+    setState(() {
+      //whereever after one action or after one click of a button , if any code  updates , then that code must be put inside the setState(){  }.  Her after every click(  onPressed(){  }  ) the question number and as well as the scoreKeeper updates , so that's why we keep both of them inside the setState()
+      if ((quizBrain.getQuestionAnswer()) == userClickedAnswer) {
+        ScoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+          size: 40.0,
+        ));
+      } else {
+        ScoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+          size: 40.0,
+        ));
+      }
+
+      //  while (QuestionNumber < 3) {
+      // print(QuestionNumber);
+      quizBrain.nextQuestion();
+      // print(QuestionNumber);
+      // }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    //Questions myQuestions = Questions();
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
+      children: [
         Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
+                quizBrain.getQuestionText(),
                 style: TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
@@ -46,52 +99,66 @@ class _QuizPageState extends State<QuizPage> {
               ),
             ),
           ),
-        ),
+        ), // For Text
         Expanded(
+          //flex: 10,
           child: Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(16.0),
             child: FlatButton(
               textColor: Colors.white,
               color: Colors.green,
               child: Text(
-                'True',
+                "True",
                 style: TextStyle(
-                  color: Colors.white,
                   fontSize: 20.0,
+                  color: Colors.white,
                 ),
               ),
+
+              //  if( Answers[QuestionNumber] == true){
+//this statement should not be written here bcz after pressing the button we will check the answer is correct or not,so we have to write this code inside the  onPressed(
+              //},
+
+              // quizBrain.QuestionBank[QuestionNumber].questionAnswer = true;     //anyone can change the code and manipulate your app.So we have to make our code private and encapsulate it.In DART we can make the code private by putting the underscore behind the variable _variable
+
               onPressed: () {
-                //The user picked true.
-              },
+                checkAnswer(true);
+              }, // this onPressed is very much important , if u don't write this FlatButton will not work
             ),
           ),
-        ),
+        ), // For True Button
         Expanded(
           child: Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(16.0),
             child: FlatButton(
+              textColor: Colors.white,
               color: Colors.red,
               child: Text(
-                'False',
+                "False",
                 style: TextStyle(
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
               ),
               onPressed: () {
-                //The user picked false.
-              },
+                checkAnswer(false);
+              }, // this onPressed is very much important , if u don't write this FlatButton will not work
             ),
           ),
-        ),
-        //TODO: Add a Row here as your score keeper
+        ), //For False Button
+        Expanded(
+          // here "Expanded" is of no use bcz it is going out of screen.So i have to fix it
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: ScoreKeeper,
+            ),
+          ),
+        ) // For Row of Score Keeper
       ],
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+// I have completed this project on my own
+//I have just seen the video upto Checking users answers (video No - 91)
---
+++ lib/question.dart
@@ -0,0 +1,16 @@
+//class Question {
+//String questionText;
+//  bool questionAnswer;
+
+// Question(String a, bool b) {
+//   questionText = a;
+//   questionAnswer = b;
+// }
+//}
+
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+}
